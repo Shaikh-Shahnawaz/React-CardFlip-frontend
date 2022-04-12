ed8db5f266c434947ccc3f8a9ec44000ae4ddb10
Shaikh Mohammed Shahnawaz <shaikhshahnawaz8821@gmail.com>
2022-04-12 23:48:39 +0530

    liked functanility added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,44 @@
 import React, { useEffect, useState } from "react";
 
 import ReactCardFlip from "react-card-flip";
+import Heart from "react-animated-heart";
+
 const Card = ({ data }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isClick, setClick] = useState(false);
+
+  const [likedData,setLikedData] = useState([])
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
   };
 
+  const handleLiked = (data)=>{
+    setClick(!isClick)
+
+    setLikedData([{name:data,isLiked:isClick}])
+
+console.log('Liked Data ==>>',likedData)
+  }
+
+  // useEffect(() => {
+
+  //   if(isClick == true){
+
+  //   }
+
+  //   localStorage.setItem()
+  // }, [isClick])
+  
+
   return (
-    <div className="my-3 col-md-4">
+    <div className="my-3 col-md-3">
       <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-        <div onClick={handleClick} class="card" style={{ width: "18rem" }}>
-          <img class="card-img-top" src={data.image} alt="Card image cap" />
-          
+        <div  class="card" style={{ width: "18rem" }}>
+          <img onClick={handleClick} class="card-img-top" src={data.image} alt="Card image cap" />
+          <p>
+          <Heart isClick={isClick} onClick={() =>handleLiked(data.name) } />
+          </p>
         </div>
 
         <div  onClick={handleClick} class="card" style={{ width: "18rem" }}>
